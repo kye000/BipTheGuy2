c62464d7d95095c21fe58eb2c2c77c2510b635d3
Kyle Sumagang <ksumagang2026@gmail.com>
2026-04-08 15:11:15 -0700

    Initial Commit

---
+++ BipTheGuy2/BipTheGuy2App.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuy2App.swift
+//  BipTheGuy2
+//
+//  Created by SUMAGANG, KYLE RUSSLIE on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BipTheGuy2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BipTheGuy2
+//
+//  Created by SUMAGANG, KYLE RUSSLIE on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
